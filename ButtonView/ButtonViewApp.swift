95f32e89cdd5a487c75b917cb3a9ad36d0cf0b05
id2k1149 <id2k1149@gmail.com>
2023-04-19 15:51:57 -0400

    Initial Commit

---
+++ ButtonView/ButtonViewApp.swift
@@ -0,0 +1,17 @@
+//
+//  ButtonViewApp.swift
+//  ButtonView
+//
+//  Created by Max Franz Immelmann on 4/19/23.
+//
+
+import SwiftUI
+
+@main
+struct ButtonViewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
